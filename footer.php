4d701643bd050f80fe8060850a04ad50f1cc5ac9
Goran Peric <goran.peric@karrieretutor.de>
2019-11-06 13:26:48 +0100

    some changes

--- footer.php
+++ footer.php
@@ -1,16 +1,10 @@
 <?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Nineteen
- * @since 1.0.0
- */
 
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+/* ###### ###### ###### ###### ###### ###### ###### ###### */
+/* ###### ###### ###### ###### ###### ###### ###### ###### */
 ?>
 
 	</div><!-- #content -->
--- header.php
+++ header.php
@@ -1,15 +1,10 @@
 <?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Nineteen
- * @since 1.0.0
- */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+/* ###### ###### ###### ###### ###### ###### ###### ###### */
+/* ###### ###### ###### ###### ###### ###### ###### ###### */
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
